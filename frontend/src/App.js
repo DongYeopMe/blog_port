2a9f2ca4544a2d8954df94b8daa37bb5d627388c
Dongyeop Lee <dlduq29@gmail.com>
2024-08-28 03:06:36 +0900

    spring boot,react 연동

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [hidata, setHello] = useState('')
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/api/hello')
+      .then(response => setHello(response.data))
+      .catch(error => console.log(error))
+  }, []);
+
   return (
-    <div className="App">
-      <p>컨텐츠</p>
+    <div>
+      백엔드 스프링 부트 데이터 : {hidata}
     </div>
   );
 }
