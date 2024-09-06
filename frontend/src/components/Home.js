7343d5cf8d2de57f0af4deddd3297b3b3d1968ed
Dongyeop Lee <dlduq29@gmail.com>
2024-09-06 23:59:29 +0900

    수정.init

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -3,41 +3,40 @@ import Profile from '../img/이력서 사진.jpg';
 function Home(){
     return(
         <div id="container" className=" h-lvh border-4 flex  flex-col ">
-            <div id="introduce " className="border-2 border-black">
-                <div id="head" className="border-2 border-black">
-                    <span>Introduce</span>
-                    <img src={Profile} className="size-40 m-2"/>
-                    <span>1996년 12월 02일</span>
-                    <div>
-                        <pre className="font-sans text-sm">
-                            언제나 발전하는 사람을 지향합니다.
-                        </pre>
+            <div id="introduce " className="h-48 grid content-center ">
+                <div id="head" className="flex justify-start h-48 ">
+                    <div className="px-8">
+                        <img src={Profile} className="size-40 m-2" />
                     </div>
-                    <div id="contact" className="border-4 border-black">
-                        <ul>
-                            <li>Blog</li>
-                            <li>Github</li>
-                            <li>Email</li>
-                            <li>Phone</li>
-                        </ul>
+                    
+                    <div id= "intro"className="flex flex-col">
+                        <span>1996년 12월 02일</span>
+                        <pre className="font-sans text-sm">
+                            완벽한 것은 존재하지 않다고 생각하기에 항상 성장하고자 노력합니다.
+                        </pre>
+                        <div id="contact">
+                            <ul>
+                                <li><a target="_blank" href="https://dongyeopme.gitbook.io/yeop-blog" id="blog">Blog</a></li>
+                                <li><a target="_blank" href="https://github.com/DongYeopMe" id="github">Github</a></li>
+                                <li>Email : dyplee22@gmail.com</li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
-
             <div id = "stacks">
-                <div id = "frame">
-                    <span>Spring</span>
-                    <span>React</span>
+                <div className="border-b-4 border-black w-5/6 p-8">
+                    <p>스택</p>
                 </div>
-                <div id="lang">
-                    <span>Java</span>
-                    <span>JavaScript</span>
-                </div>
-                <div id="others">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-
+                <div id = "frame" className="">
+                    <pre>
+                        ― 언어
+                        ┖ Java / JavaScript
+                        ― 프론트,백엔드
+                        ┖ React / Spring Boot / JPA / MySQL / Redis
+                        ― 기타
+                        ┖ AWS / Docker / Git
+                    </pre>
                 </div>
             </div>
             <div id ="projects">
