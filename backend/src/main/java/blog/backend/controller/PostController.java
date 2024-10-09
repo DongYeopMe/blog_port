feff17f2afe08023d866177f824cb7e585b08579
Dongyeop Lee <dlduq29@gmail.com>
2024-10-09 22:46:00 +0900

    post api init

--- backend/src/main/java/blog/backend/controller/PostController.java
+++ backend/src/main/java/blog/backend/controller/PostController.java
@@ -2,9 +2,8 @@ package blog.backend.controller;
 
 import blog.backend.service.PostService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -12,5 +11,28 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostController {
     private final PostService postService;
 
-    @PostMapping("/")
+    @PostMapping("/create")
+    public String createPost(){
+        return "post ok";
+    }
+    @PatchMapping("/update")
+    public String updatePost(){
+        return "edit ok";
+    }
+    @GetMapping("/get")
+    public String getPost(){
+        return "get ok";
+    }
+    @GetMapping("/gets")
+    public String getPosts(){
+        return "gets ok";
+    }
+    @DeleteMapping("/delete")
+    public String deletePost(){
+        return "delete ok";
+    }
+    @DeleteMapping("/deteles")
+    public String deletePosts(){
+        return "deletes ok";
+    }
 }
